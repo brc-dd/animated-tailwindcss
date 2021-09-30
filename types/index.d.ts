62a5a3677647a2f81074cadc8f22b90dab54dd9d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-09-30 23:58:30 +0530

    fix: major refactor

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,25 @@
 declare module 'tailwindcss/lib/util/createUtilityPlugin' {
-  const createUtilityPlugin: (
-    themeKey: string,
-    utilityVariations?: Array<unknown>,
-    _?: { filterDefault?: boolean; type?: string },
-  ) => unknown;
+  const createUtilityPlugin: (themeKey: string, utilityVariations?: Array<unknown>) => unknown;
   export = createUtilityPlugin;
 }
+
+declare module 'tailwindcss/plugin' {
+  type PluginFunction = { addUtilities: (u: CSSBlock) => void };
+  const createPlugin: (plugin: (pluginFunction: PluginFunction) => void) => unknown;
+  export = createPlugin;
+}
+
+type TailwindConfig =
+  | Partial<import('tailwindcss/tailwind-config').TailwindConfig> & { mode?: 'jit' };
+
+type CSSBlock = Record<string, CSSProperties>;
+
+type CSSExtensions =
+  | Record<`--${string}`, number | string>
+  | Record<`@apply ${string}`, Record<string, never>>;
+
+type CSSProperties = CSSExtensions & import('csstype').Properties;
+
+type Keyframes = Record<string, CSSBlock>;
+
+type Main = (config: TailwindConfig, _: { experimental?: boolean }) => TailwindConfig;
